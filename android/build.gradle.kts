06f42f2e14caacb4501ae9d53d84ed43179f421a
sailing-innocent <sailing-innocent@foxmail.com>
2026-08-29 09:58:27 +0800

    remove lark skills and update gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 // 根构建脚本：仅声明插件版本（apply false），子模块按需应用
 // 版本选型见 doc/design/android_app/ACCEPTANCE_M1.md 附录
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    id("com.android.application") version "9.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
